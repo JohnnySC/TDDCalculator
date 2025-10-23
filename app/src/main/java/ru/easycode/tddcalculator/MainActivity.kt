7ee064b5988cd8c6a9633af0678cb0bd0d25d79e
hovhannes <johnnysc091@gmail.com>
2025-10-23 23:21:10 +0400

    step 2: MainViewModel

--- app/src/main/java/ru/easycode/tddcalculator/MainActivity.kt
+++ app/src/main/java/ru/easycode/tddcalculator/MainActivity.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import ru.easycode.tddcalculator.ui.theme.TDDCalculatorTheme
 
 class MainActivity : ComponentActivity() {
+
+    val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,9 +23,18 @@ class MainActivity : ComponentActivity() {
         setContent {
             TDDCalculatorTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
+                    Box(Modifier.padding(innerPadding)) {
+                        val inputFlow = viewModel.inputFlow.collectAsStateWithLifecycle()
+                        val resultFlow = viewModel.resultFlow.collectAsStateWithLifecycle()
+                        MainScreen(
+                            input = inputFlow.value,
+                            result = resultFlow.value,
+                            actions = viewModel
+                        )
+                    }
                 }
             }
         }
     }
 }
+
