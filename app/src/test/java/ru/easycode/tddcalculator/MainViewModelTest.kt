7ab4cf67878fb1868b4736bfb7ac2e4094adb6d7
hovhannes <johnnysc091@gmail.com>
2025-10-23 22:52:08 +0400

    step 1: MainViewModelTest

--- app/src/test/java/ru/easycode/tddcalculator/MainViewModelTest.kt
+++ app/src/test/java/ru/easycode/tddcalculator/MainViewModelTest.kt
@@ -1,11 +1,38 @@
 package ru.easycode.tddcalculator
 
+import kotlinx.coroutines.flow.StateFlow
+import org.junit.Assert.assertEquals
+import org.junit.Before
 import org.junit.Test
 
 class MainViewModelTest {
 
-    @Test
-    fun scenario_number_one() {
+    private lateinit var viewModel: MainViewModel
 
+    @Before
+    fun setup() {
+        viewModel = MainViewModel()
+    }
+
+    @Test
+    fun sum_of_two_numbers() {
+        val inputFlow: StateFlow<String> = viewModel.inputFlow
+        val resultFlow: StateFlow<String> = viewModel.resultFlow
+
+        assertEquals("", inputFlow.value)
+        assertEquals("", resultFlow.value)
+
+        viewModel.inputOne()
+        assertEquals("1", inputFlow.value)
+
+        viewModel.plus()
+        assertEquals("1+", inputFlow.value)
+
+        viewModel.inputTwo()
+        assertEquals("1+2", inputFlow.value)
+
+        viewModel.calculate()
+        assertEquals("1+2", inputFlow.value)
+        assertEquals("3", resultFlow.value)
     }
 }
