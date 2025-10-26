8e140fe34ccdfa5066e3b51a6a610269ea4f3dc3
hovhannes <johnnysc091@gmail.com>
2025-10-26 14:14:56 +0400

    prevent leading zeros step 1: ui test

--- app/src/androidTest/java/ru/easycode/tddcalculator/ScenarioUiTest.kt
+++ app/src/androidTest/java/ru/easycode/tddcalculator/ScenarioUiTest.kt
@@ -76,4 +76,13 @@ class ScenarioUiTest {
         mainPage.assertInputField(expected = "0+0")
         mainPage.assertResult(expected = "0")
     }
+
+    @Test
+    fun prevent_leading_zeros() {
+        mainPage.clickNumberZero()
+        mainPage.assertInputField(expected = "0")
+
+        mainPage.clickNumberOneButton()
+        mainPage.assertInputField(expected = "1")
+    }
 }
