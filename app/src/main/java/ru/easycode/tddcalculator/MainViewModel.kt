0255e93f6a1f5c02d993a227d82b77cf8c2b6d2a
hovhannes <johnnysc091@gmail.com>
2025-10-26 14:26:59 +0400

    prevent leading zeros step 4: viewModel fix

--- app/src/main/java/ru/easycode/tddcalculator/MainViewModel.kt
+++ app/src/main/java/ru/easycode/tddcalculator/MainViewModel.kt
@@ -28,9 +28,12 @@ class MainViewModel : ViewModel(), MainActions {
     }
 
     override fun inputTwo() {
-        right += "2"
-        val before = inputFlow.value
-        inputMutableFlow.value = before + right
+        if (right == "0") {
+            right = "2"
+        } else {
+            right += "2"
+        }
+        inputMutableFlow.value = "$left+$right"
     }
 
     override fun inputZero() {
